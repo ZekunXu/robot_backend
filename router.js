130e46bba6d6b81631149132c0055323de623cca
Victor <victor950602@gmail.com>
2020-12-22 15:05:34 +0800

    v0.0.20201222.2

--- router.js
+++ router.js
@@ -8,6 +8,7 @@ router
 	.get('/api/robot/webcams', WebCamController.getAllUrls)
 	.post('/api/robot/webcams', WebCamController.getUrlByHardwareID)
 	.post('/api/robot/webcams/save', WebCamController.saveWebCam)
+	.post('/api/robot/webcams/delete', WebCamController.deleteWebCam)
 
 
 module.exports = router;
