889a6eb6846f6f00a7d4de7fc7b38907454eac02
Victor <victor20111010@163.com>
2021-01-31 18:34:16 +0800

    v0.0.20210130

--- test.js
+++ test.js
@@ -1,42 +1,24 @@
-const GLOBAL_PARAM = require("./lib/global_param.js");
-const axios = require("axios");
 
 
+var abc = "abc";
 
 Promise
     .resolve()
     .then(()=>{
-        const data = {
-            platform: "all",
-            audience: {
-                registration_id: [
-                    "170976fa8a274b3356a"
-                ]
-            },
-            notification: {
-                android: {
-                    alert: "没想到吧，这是一条推送,消息很长。\n 还能分段 \n 分段 \n 分段",
-                    title: "标题标题",
-                    big_text: "没想到吧，这是一条推送,消息很长。\n 还能分段 \n 分段 \n 分段",
-                    priority: 1,
-                    style: 1
-                }
-            }
-        }
-
-        return axios.post(
-            GLOBAL_PARAM.JG_PUSH_URL, 
-            {data: data}, 
-            {
-                auth: {
-                    username: GLOBAL_PARAM.JG_APP_KEY,
-                    password: GLOBAL_PARAM.JG_MASTER_SECRET,
-                }
-            });
+        console.log(1);
     })
-    .then((value)=>{
-        console.log("value: " + value);
+    .then(()=>{
+        console.log(2);
+        abc = "123";
+    })
+    .then(()=>{
+        console.log(abc);
+        console.log(3);
     })
     .catch((err)=>{
-        console.log("err: " + err);
+        switch(err){
+            case 101:
+                console.log(4);
+                break;
+        }
     })
