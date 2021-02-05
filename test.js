0e02abcea4d5d63f49888e8ef7f0b888c88e45ad
Victor <victor20111010@163.com>
2021-02-05 09:56:56 +0800

    v2021.2.5

--- test.js
+++ test.js
@@ -1,24 +1,41 @@
 
-
-var abc = "abc";
+const axios = require("axios");
+const GLOBAL_PARAM = require(("./lib/global_param.js"));
 
 Promise
-    .resolve()
-    .then(()=>{
-        console.log(1);
-    })
-    .then(()=>{
-        console.log(2);
-        abc = "123";
-    })
-    .then(()=>{
-        console.log(abc);
-        console.log(3);
-    })
-    .catch((err)=>{
-        switch(err){
-            case 101:
-                console.log(4);
-                break;
+.resolve()
+.then(()=>{
+    const data = {
+        platform: "all",
+        audience: {
+            registration_id: [
+                "dsadsdas"
+            ]
+        },
+        notification: {
+            android: {
+                alert: "abc",
+                title: "",
+                big_text: "",
+                priority: 1,
+                style: 1
+            }
         }
-    })
+    }
+
+    return axios.post(
+        GLOBAL_PARAM.JG_PUSH_URL, 
+        data, 
+        {
+            auth: {
+                username: GLOBAL_PARAM.JG_APP_KEY,
+                password: GLOBAL_PARAM.JG_MASTER_SECRET,
+            }
+        });
+})
+.then((value)=>{
+    console.log("发送了一条通知: " + JSON.stringify(value.data));
+})
+.catch((err)=>{
+    console.log("err: " + err);
+})
