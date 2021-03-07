9c9a630aaf34e63c678c29c9dc38936a10703241
Victor <victor950602@gmail.com>
2021-03-07 21:11:42 +0800

    v1.4build1的第一个接口，哈哈哈哈

--- test.js
+++ test.js
@@ -1,5 +1,69 @@
 
+// 弹窗接口
+var json = {
+    "statusCode": "connected",
+    "msg": "success",
+    "param": [
+        {
+            "programName": "IFS 国金中心",
+            "programCode": "IFS",
+            "GPSLocation": {
+                "latitute": 20.0,
+                "longitude": 108.123
+            },
+            "devices": [
+                {
+                    "hardwareType": "wwwRobot",
+                    "hardwareName": "万维机器人",
+                    "count": 4
+                },
+                {
+                    "hardwareType": "HaikangWebCam",
+                    "hardwareName": "固定摄像头",
+                    "count": 20,
+                }
+            ]
+        },
+        {
+            "programName": "乔庄社区 56号",
+            "programCode": "QZSQ",
+            "GPSLocation": {
+                "latitute": 20.0,
+                "longitude": 108.123
+            },
+            "devices": [
+                {
+                    "hardwareType": "Robot",
+                    "hardwareName": "市井机器人",
+                    "count": 1
+                },
+                {
+                    "hardwareType": "HaikangWebCam",
+                    "hardwareName": "固定摄像头",
+                    "count": 20,
+                }
+            ]
+        },
+    ]
+}
 
-var abc = {a: 1, b: 2};
 
-console.log(JSON.stringify(abc));
+//首页加载接口
+// request api
+var json = {
+    "programCode": "IFS",
+}
+
+// response api
+var json = {
+    "statusCode": "",
+    "msg": "",
+    "param": [
+        {
+
+        },
+        {
+            
+        }
+    ]
+}
