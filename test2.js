96a72e04bec0ba4f04d6df5bcddf4593a3dee32a
victor <victor20111010@163.com>
2021-05-14 19:56:19 +0800

    更新获取设备状态 api

--- test2.js
+++ test2.js
@@ -1,5 +1,31 @@
-var abc = "hahahha";
+const axios = require('axios');
+const GLOBAL_PARAM = require('./lib/global_param.js');
 
-JSON.stringify(abc).replace(/"/g, "").replace(/\'/g, "\"");
 
-console.log(abc);
+
+
+
+async function abc () {
+
+    await axios.post(
+        "http://localhost:3000/api/updateDB/insert", 
+        {
+            "appName": "test",
+            "version": "1.0",
+            "build": -1
+        },
+        {
+            headers: {authorization: `Bearer ${GLOBAL_PARAM.BEARTOKEN_FOR_DB}`}
+        }
+    )
+    .then((value)=>{
+        console.log(value.data);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+
+}
+
+abc ();
